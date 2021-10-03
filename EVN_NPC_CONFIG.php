2a03334d11c61950f1cdc6a9a7f87028c899f5db
Marion001 <27297851+marion001@users.noreply.github.com>
2021-10-03 19:59:57 +0700

    Update EVN_NPC_CONFIG.php

--- EVN_NPC_CONFIG.php
+++ EVN_NPC_CONFIG.php
@@ -5,7 +5,10 @@
 #Page: https://www.facebook.com/Party.Marion002
 error_reporting(0);
 @date_default_timezone_set('Asia/Ho_Chi_Minh');
-$MaKhachHang = "PA***********";  // Thay Mã Khách Hàng Của Bạn Tại Đây PA***********
+$MaKhachHang = "PA23*********";  // Thay Mã Khách Hàng Của Bạn Tại Đây PA***********
+$SoPha = "1";
+$MaDiemDo = "$MaKhachHang"."001";
+$MaDVQuanLy = substr("$MaKhachHang", 0, 6);
 $SetNgayThang = date("d-m-Y"); 
 $SetThoiGian = date("H:i"); 
 $NGay_Bat_Dau = date_format(date_modify(date_create("$SetNgayThang"), "-3 days"), "Y-m-d");
@@ -13,5 +16,7 @@ $NGay_Ket_Thuc = date_format(date_modify(date_create("$SetNgayThang"), "-1 days"
 $API_Get_DD_Info = base64_decode("aHR0cHM6Ly9hcGlucGMuZWR1a2l0ZWFwcC5vbmxpbmUvbW9iaWxlYXBpL2hvLXNvL0RET19JTkZPLw==")."$MaKhachHang";
 $API_Get_Info_KH = base64_decode("aHR0cHM6Ly9hcGlucGMuZWR1a2l0ZWFwcC5vbmxpbmUvbW9iaWxlYXBpL2hvbWUv")."$MaKhachHang";
 $API_Co_Mat_Dien = base64_decode("aHR0cHM6Ly9hcGlucGMuZWR1a2l0ZWFwcC5vbmxpbmUvbW9iaWxlYXBpL3Rob25nLXRpbi1jYXQtZGllbi9nZXQ=");
-$API_San_Luong_Ngay = base64_decode("aHR0cHM6Ly9hcGlucGMuZWR1a2l0ZWFwcC5vbmxpbmUvbW9iaWxlYXBpL3Nhbi1sdW9uZy1kaWVuL3RyYS1jdXU=");
+//$API_San_Luong_Ngay = base64_decode("aHR0cHM6Ly9hcGlucGMuZWR1a2l0ZWFwcC5vbmxpbmUvbW9iaWxlYXBpL3Nhbi1sdW9uZy1kaWVuL3RyYS1jdXU=");
+
+
 ?>
