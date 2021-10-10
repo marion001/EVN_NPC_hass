82c321d9f7e2d39f84678984791cee144667fdd7
Marion001 <27297851+marion001@users.noreply.github.com>
2021-10-10 11:32:17 +0700

    Update EVN_NPC_CONFIG.php

--- EVN_NPC_CONFIG.php
+++ EVN_NPC_CONFIG.php
@@ -11,12 +11,9 @@ $MaDiemDo = "$MaKhachHang"."001";
 $MaDVQuanLy = substr("$MaKhachHang", 0, 6);
 $SetNgayThang = date("d-m-Y"); 
 $SetThoiGian = date("H:i"); 
-$NGay_Bat_Dau = date_format(date_modify(date_create("$SetNgayThang"), "-3 days"), "Y-m-d");
-$NGay_Ket_Thuc = date_format(date_modify(date_create("$SetNgayThang"), "-1 days"), "Y-m-d");
+$TinhToanNgayThang = date_create("$SetNgayThang");
+$NgayChotBatDau = date_format(date_modify(date_create("$SetNgayThang"), "-4 days"), "d-m-Y");
 $API_Get_DD_Info = base64_decode("aHR0cHM6Ly9hcGlucGMuZWR1a2l0ZWFwcC5vbmxpbmUvbW9iaWxlYXBpL2hvLXNvL0RET19JTkZPLw==")."$MaKhachHang";
 $API_Get_Info_KH = base64_decode("aHR0cHM6Ly9hcGlucGMuZWR1a2l0ZWFwcC5vbmxpbmUvbW9iaWxlYXBpL2hvbWUv")."$MaKhachHang";
 $API_Co_Mat_Dien = base64_decode("aHR0cHM6Ly9hcGlucGMuZWR1a2l0ZWFwcC5vbmxpbmUvbW9iaWxlYXBpL3Rob25nLXRpbi1jYXQtZGllbi9nZXQ=");
-//$API_San_Luong_Ngay = base64_decode("aHR0cHM6Ly9hcGlucGMuZWR1a2l0ZWFwcC5vbmxpbmUvbW9iaWxlYXBpL3Nhbi1sdW9uZy1kaWVuL3RyYS1jdXU=");
-
-
 ?>
